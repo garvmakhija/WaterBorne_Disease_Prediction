c901615672f0b5c20d147434c21e4cb6bbbe0dc7
Garv <garvmakhija2006@gmail.com>
2026-06-28 20:39:01 +0530

    Updated API URL for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://waterborne-disease-prediction-7ds0.onrender.com",
 });
 
 export default API;
